966aa8a0530cf760f2c87059032b8b2a3097c3a2
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2023-02-01 21:31:56 -0500

    The mod

--- README.md
+++
@@ -1,1 +0,0 @@
-Once cloned, rename `src/TestMod.csproj`, then edit `assets/modinfo.json` and `src/Plugin.cs` to customize your mod.
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,32 +1,44 @@
-﻿global using System;
-global using System.Linq;
-global using System.Text;
-global using System.Collections.Generic;
-global using UnityEngine;
-
-using BepInEx;
+﻿using BepInEx;
 using System.Security.Permissions;
 
 #pragma warning disable CS0618 // Do not remove the following line.
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 
-namespace TestMod;
+namespace SunHat;
 
-// Add target_game_version and youtube_trailer_id to modinfo.json if applicable.
-// See https://rainworldmodding.miraheze.org/wiki/Downpour_Reference/Mod_Directories
-
-[BepInPlugin("com.author.testmod", "Test Mod", "0.1.0")]
+[BepInPlugin("com.dual-iron.sunhat", "Sun Hat", "1.0.0")]
 sealed class Plugin : BaseUnityPlugin
 {
+    FAtlas atlas;
+
     public void OnEnable()
     {
         On.RainWorld.OnModsInit += Init;
+        On.PlayerGraphics.DrawSprites += PlayerGraphics_DrawSprites;
     }
 
     private void Init(On.RainWorld.orig_OnModsInit orig, RainWorld self)
     {
         orig(self);
 
-        Logger.LogDebug("Hello world!");
+        atlas = Futile.atlasManager.LoadAtlas("sprites/sunhat");
+
+        if (atlas == null) {
+            Logger.LogWarning("Sun Hat atlas not found! Reinstall the mod.");
+        }
+    }
+
+    private void PlayerGraphics_DrawSprites(On.PlayerGraphics.orig_DrawSprites orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, UnityEngine.Vector2 camPos)
+    {
+        orig(self, sLeaser, rCam, timeStacker, camPos);
+
+        if (atlas == null) {
+            return;
+        }
+
+        string name = sLeaser.sprites[3]?.element?.name;
+        if (name != null && name.StartsWith("HeadA") && atlas._elementsByName.TryGetValue("Sun" + name, out var element)) {
+            sLeaser.sprites[3].element = element;
+        }
     }
 }
